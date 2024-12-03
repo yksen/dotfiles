6dffd009c402796f0fa4a83dd9b3eea55d73c0b8
xen <selegakamil@gmail.com>
2024-12-03 13:45:28 +0100

    Add vim-visual-multi

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -230,6 +230,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	"mg979/vim-visual-multi",
 
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
