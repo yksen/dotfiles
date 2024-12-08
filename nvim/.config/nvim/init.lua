f155d77a6f09b385bb79f85f03a2d7a41b1eebd6
xen <selegakamil@gmail.com>
2024-12-08 12:35:24 +0100

    Add `.editorconfig`

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -230,7 +230,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-	"mg979/vim-visual-multi",
+
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
 	-- keys can be used to configure plugin behavior/loading/etc.
@@ -957,6 +957,8 @@ require("lazy").setup({
 	-- require 'kickstart.plugins.autopairs',
 	-- require 'kickstart.plugins.neo-tree',
 	-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+	"mg979/vim-visual-multi",
+	"gpanders/editorconfig.nvim",
 
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
