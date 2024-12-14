5837a7aca08a7e09aef469ae691957ce4371e246
xen <selegakamil@gmail.com>
2024-12-14 21:43:20 +0100

    Add `bashls`

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -625,6 +625,7 @@ require("lazy").setup({
 				-- gopls = {},
 				-- pyright = {},
 				rust_analyzer = {},
+				bashls = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
