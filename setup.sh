08bc6e9a711794a8b6d197ca1ccc9b3d691acd5d
xen <selegakamil@gmail.com>
2024-12-14 23:53:17 +0100

    Refactor `setup.sh`

--- setup.sh
+++ setup.sh
@@ -2,29 +2,17 @@
 
 shopt -s globstar
 
-while getopts ":r" option; do
-    case $option in
-        r)
-        RUN=true;;
-    esac
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+SOURCE_DIR="$HOME"
+TARGET_DIRS=$(ls "$SCRIPT_DIR"/*/ -d)
+
+EXCLUDES=(-not -path "*powershell*" -not -path "*AppData*")
+
+for TARGET_DIR in $TARGET_DIRS; do
+    TARGET_FILES=$(find "$TARGET_DIR" -type f "${EXCLUDES[@]}")
+    for TARGET_FILE in $TARGET_FILES; do
+        SOURCE_PATH=$SOURCE_DIR/$(echo "${TARGET_FILE/$SOURCE_DIR/}" | cut -d '/' -f4-)
+        mkdir -p "$(dirname "${SOURCE_PATH}")"
+        ln -sf "$TARGET_FILE" "$SOURCE_PATH"
+    done
 done
-
-SOURCE_DIR="$(realpath .)"
-TARGET_DIR="$HOME"
-
-for file in $(find $SOURCE_DIR -type f -name "*" -not -name "README.md" -not -path "*/setup*" -not -path "*/.git/*" -not -path "*/powershell/*"); do
-    if [[ -f "$file" ]]; then
-        TARGET_PATH=$file
-        SOURCE_PATH=$HOME/$(echo ${file/$SOURCE_DIR} | cut -d'/' -f3-)
-        if [[ $RUN = true ]]; then
-            mkdir -p $(dirname $SOURCE_PATH)
-            ln -sf "$TARGET_PATH" "$SOURCE_PATH"
-        else
-            echo "ln -sf $TARGET_PATH $SOURCE_PATH";
-        fi
-    fi
-done
-
-if [[ -z ${RUN+x} ]]; then
-    echo "This was a dry run, use with -r to execute"
-fi
