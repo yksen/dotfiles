a239d609bda8b6b04f3b3b77e8b55af6aff08b15
xen <selegakamil@gmail.com>
2025-01-02 15:47:54 +0100

    Fix `setup.sh` for paths with spaces

--- setup.sh
+++ setup.sh
@@ -10,9 +10,12 @@ EXCLUDES=(-not -path "*powershell*" -not -path "*AppData*")
 
 for TARGET_DIR in $TARGET_DIRS; do
     TARGET_FILES=$(find "$TARGET_DIR" -type f "${EXCLUDES[@]}")
-    for TARGET_FILE in $TARGET_FILES; do
-        SOURCE_PATH=$SOURCE_DIR/$(echo "${TARGET_FILE/$SOURCE_DIR/}" | cut -d '/' -f4-)
+    if [ -z "$TARGET_FILES" ]; then
+        continue
+    fi
+    while IFS= read -r TARGET_FILE; do
+        SOURCE_PATH="$SOURCE_DIR/$(echo "${TARGET_FILE/$SOURCE_DIR/}" | cut -d '/' -f4-)"
         mkdir -p "$(dirname "${SOURCE_PATH}")"
         ln -sf "$TARGET_FILE" "$SOURCE_PATH"
-    done
+    done <<<"$TARGET_FILES"
 done
