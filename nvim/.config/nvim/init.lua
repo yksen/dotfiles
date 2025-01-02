fa13803a1a5cb10f5d4ec925ea4a136026e27202
xen <selegakamil@gmail.com>
2025-01-02 15:29:12 +0100

    Add 'shellcheck'

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -665,6 +665,7 @@ require("lazy").setup({
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
 				"clang-format",
+				"shellcheck",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
