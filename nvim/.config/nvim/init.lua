61268d088c071192a88279f4d7e6cf6654f55461
xen <selegakamil@gmail.com>
2025-01-23 21:22:24 +0100

    Switch colorscheme to `cyberdream`

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -848,13 +848,15 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
 		priority = 1000, -- Make sure to load this before all the other start plugins.
+		opts = {},
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("cyberdream")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
