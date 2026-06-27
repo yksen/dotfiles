b9919032ddc3723676846d0ae9d575ab87982a48
xen <selegakamil@gmail.com>
2026-06-27 15:20:14 +0200

    [nvim] Add all files live grep

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -351,6 +351,12 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", "<leader>sG", function()
+				builtin.live_grep({
+					additional_args = { "--hidden", "--no-ignore" },
+					prompt_title = "Live Grep (all files)",
+				})
+			end, { desc = "[S]earch by [G]rep (all files)" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
